b858c0028a3fda59b757ef82eecba9fa9f5d037a
Steven Clontz <steven.clontz@gmail.com>
2015-02-21 11:44:04 -0600

    simply return JSON with latest posts

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -6,11 +6,21 @@ require 'twitter'
 class TwitterFetcher < Sinatra::Base
   helpers Sinatra::Jsonp
 
+  # store development keys in gitignored files
+  if Sinatra::Base.development?
+    consumer_secret = File.read('.twitter_consumer_secret').strip
+    oauth_secret = File.read('.twitter_oauth_secret').strip
+  # store production keys in environment variables
+  else
+    consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+    oauth_secret = ENV['TWITTER_OAUTH_SECRET']
+  end
+
   @@twitter_client = Twitter::Client.new(
-    :consumer_key       => 'YOUR-CONSUMER-KEY-JUNX',
-    :consumer_secret    => 'YOUR-CONSUMER-SECRET-JUNX',
-    :oauth_token        => 'YOUR-OAUTH-TOKEN-JUNX',
-    :oauth_token_secret => 'YOUR-OAUTH-TOKEN-SECRET-JUNX',
+    :consumer_key       => 'zHdSQhFBWP3w2MtLYqvejrJcH',
+    :consumer_secret    => consumer_secret,
+    :oauth_token        => '3048458734-W2JDpijwRaJ8GWAMzWDV4ErPfwSI8hGADgXLgCX',
+    :oauth_token_secret => oauth_secret,
   )
 
   get '/' do
